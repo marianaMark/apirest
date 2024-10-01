036e3d564e261df33dc8f6c56ecf83e1656b61d5
Mariana Marca <mariana.marca.herrera.2003@gmail.com>
2024-10-01 06:23:04 -0400

    complementacion

--- index.php
+++ index.php
@@ -3,6 +3,9 @@
 require_once "controlador/rutas.controlador.php";
 require_once "controlador/cursos.controlador.php";
 
+//MODELOS
+require_once "modelo/cursos.modelo.php";
+
 $rutas=new ControladorRutas();
 $rutas->inicio();
 
